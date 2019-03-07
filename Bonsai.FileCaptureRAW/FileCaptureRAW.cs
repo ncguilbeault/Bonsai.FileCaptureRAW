61818ca6802209269676b8da9dd054c19cbfcdc7
ncguilbeault <nicholas.guilbeault@mail.utoronto.ca>
2019-03-07 13:12:49 -0500

    Initial commit.

---
+++ Bonsai.FileCaptureRAW/FileCaptureRAW.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.Threading;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+using System.Drawing.Design;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Diagnostics;
+
+
+namespace Bonsai.FileCaptureRAW
+{
+    public class FileCaptureRAW : Source<IplImage>
+    {
+
+        public override IObservable<IplImage> Generate()
+        {
+            return source;
+        }
+
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        [Description("The name of the movie file.")]
+        public string FileName { get; set; }
+
+        public double FrameRate { get; set; }
+
+        public bool Replay { get; set; }
+
+        public int StartPosition { get; set; }
+
+        private IObservable<IplImage> source;
+
+        public FileCaptureRAW()
+        {
+            source = Observable.Create<IplImage>((observer, cancellationToken) =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    string fileName = FileName;
+                    double frameRate = FrameRate;
+                    bool replay = Replay;
+                    int startPosition = StartPosition;
+                    Capture capture = Capture.CreateFileCapture(fileName);
+                    double frameCount = capture.GetProperty(CaptureProperty.FrameCount);
+                    int i;
+                    if (startPosition < 0 || startPosition >= frameCount)
+                    {
+                        startPosition = 0;
+                    }
+                    i = startPosition;
+                    StartPosition = i;
+                    if (frameRate <= 0)
+                    {
+                        frameRate = capture.GetProperty(CaptureProperty.Fps);
+                        FrameRate = frameRate;
+                    }
+                    IplImage frame;
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        capture.SetProperty(CaptureProperty.PosFrames, i);
+                        if (i < frameCount - 1)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            if (replay)
+                            {
+                                i = startPosition;
+                            }                       
+                        }
+                        frame = capture.QueryFrame().Clone();
+                        observer.OnNext(frame);
+                        Thread.Sleep((int) (1000 / frameRate));
+                    }
+                    observer.OnCompleted();
+                    capture.Dispose();
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+            })
+            .PublishReconnectable()
+            .RefCount();
+        }
+    }
+}
